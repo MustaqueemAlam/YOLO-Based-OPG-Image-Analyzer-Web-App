b004ca994605ea086d575c70928597d9360f78fa
Mustaqueem <131261010+MustaqueemAlam@users.noreply.github.com>
2025-12-02 06:30:44 +0600

    updated ui

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { spawn } = require('child_process');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static('public'));
 
